2bd8165738c3760382a219f81ce551b1af1a3e8a
Ante Budimir <developer@antebudimir.com>
2022-06-02 17:19:02 +0300

    test todos added

---
+++ src/hooks/tests/useAxios.test.tsx
@@ -0,0 +1,22 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import useAxios from '../useAxios';
+
+// export default {
+// 	get: jest.fn(() => Promise.resolve({ data: [] })),
+// };
+
+describe('useAxios custom hook', () => {
+	test.todo('fetches recipes by correct url passed via endpoint prop');
+});
+
+describe('While fetching data', () => {
+	test.todo('if it handles loading state correctly');
+});
+
+describe('When data fetched successfully', () => {
+	test.todo('if it handles successful state correctly');
+});
+
+describe('An error happens during request', () => {
+	test.todo('if it handles error state correctly');
+});
